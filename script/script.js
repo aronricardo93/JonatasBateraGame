865e9e5357c3b00e4e38bd027bf59cc931dc00b1
Aron Ricardo <aronricardo@hotmail.com>
2022-07-09 05:02:58 -0300

    implementação script

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 const sprite = document.querySelector('.sprite-jonatas');
+const obstaculo = document.querySelector('.obstaculo');
 
 const jump = () => {
     sprite.classList.add('jump');
@@ -8,5 +9,18 @@ const jump = () => {
         
     }, 1000);
 }
+
+const loop = setInterval(() => {
+    const obstaculoPosition = obstaculo.offsetLeft;
+    const spritePosition = +window.getComputedStyle(sprite).bottom.replace('px', '');
+
+    if(obstaculoPosition <= 320 && obstaculoPosition > 0 && spritePosition < 50){
+        obstaculo.style.animation = 'none';
+        obstaculo.style.left = `${obstaculoPosition}px`;
+        
+        sprite.style.animation = 'none';
+        sprite.style.bottom = `${spritePosition}px`;
+    }
+}, 10);
 
 document.addEventListener('keydown', jump);
